6a10858fbf03f9e16a138be8765e2cf32986a04f
acemaster <vivekhtc25@gmail.com>
2016-04-22 13:23:16 +0530

    SLR bugs

--- slrparsing.cpp
+++ slrparsing.cpp
@@ -18,24 +18,24 @@ void addtoprodmap(string lprod,vector<string> prod)
 
 class Production
 {
-	int index;
-	string lprod;
-	vector<string> production;
 	public:
+		int index;
+		string lprod;
+		vector<string> production;
 		Production()
 		{
 			index=-1;
 		}
 		void addForProd(int index1,string lprod1,vector<string> production1)
 		{
-			cout<<"\n\t\t\tCreating new production .....";
+			// cout<<"\n\t\t\tCreating new production .....";
 			index = index1;
 			lprod = ""+lprod1;
 			for(int i=0;i<production1.size();i++)
 			{
 				production.push_back(production1[i]);
 			}
-			cout<<"\n\t\t\tDone production\n";
+			// cout<<"\n\t\t\tDone production\n";
 		}
 		void insertprod()
 		{
@@ -108,7 +108,7 @@ class Production
 		{
 			cout<<"\tCreating new production for string "<<a;
 			Production p;
-			if(production[index].compare(a) == 0)
+			if(index < production.size() && production[index].compare(a) == 0)
 				{
 					cout<<"\n\t\tNew production ....";
 					Production p;
@@ -128,7 +128,7 @@ class CSI
 		vector<Production> prods;
 		CSI()
 		{
-			cout<<"New CSI";
+			cout<<"New CSI\n";
 		}
 		CSI(vector<Production> prods)
 		{
@@ -156,17 +156,25 @@ class CSI
 				cout<<"\nfor prod ......"<<i<<endl;
 				Production p=prods[i].newprod(s);
 				string nlprod= p.getdotstring();
+				cout<<"\n====Finished normal creations======\n";
+				p.viewproduction();
 				if(p.getindex() != -1)
+				{
+					cout<<"Pushing...... new csi";
 			    	c.prods.push_back(p);
+				}
+			    
 			    if(prodmap.count(nlprod) > 0)
 			    {
-			    	for(int j=0;j<prodmap[nlprod].size();i++)
+			    	for(int j=0;j<prodmap[nlprod].size();j++)
 			    	{
 			    		Production temp;
 			    		temp.addForProd(0,nlprod,prodmap[nlprod][j]);
 			    		c.prods.push_back(temp);
 			    	}
 			    }
+			    cout<<"\n======Finished prodmap addition\n";
+			    c.viewcsi();
 			}
 			return c;
 		}
@@ -180,7 +188,16 @@ class CSI
 				cout<<endl;
 			}
 		}
-		
+		int compareCSI(CSI temp)
+		{
+			for(int j=0;j<temp.prods.size();j++)
+				for(int i=0;i<prods.size();i++)
+				{
+					if(temp.prods[j].checkprod(prods[i]) == true)
+						return 1;
+				}
+			return 0;
+		}
 		vector<string> startstrings()
 		{
 			vector<string> sstrings;
@@ -193,7 +210,7 @@ class CSI
 							flag=1;
 							break;
 						}
-				if(flag == 0)
+				if(flag == 0 && prods[i].getdotstring().length() > 0)
 				{
 					cout<<"Pushing string: "<<prods[i].getdotstring()<<endl;
 					sstrings.push_back(prods[i].getdotstring());
@@ -205,11 +222,11 @@ class CSI
 
 class CSIMap
 {
+public:
 	int s1;
 	int s2;
 	int state;
 	string movingvariable;
-public:
 	void addCSIMap(int i,int j,int st,string mv)
 	{
 		s1=i;
@@ -227,10 +244,40 @@ public:
 vector<CSIMap> csimap;
 
 
+// void viewTable()
+// {
+// 	vector<string> terminals;
+// 	vector<string> nonterminals;
+// 	for(int i=0;i<csimap.size();i++)
+// 		if(csimap[i].state == 1)
+// 			terminals.push_back(csimap[i].movingvariable);
+// 		else
+// 			nonterminals.push_back(csimap[i].movingvariable);
+// 	cout<<"\n=========================================================================================\n";
+// 	cout<<"\t\t";
+// 	for(int i=0;i<terminals.size();i++)
+// 		cout<<terminals[i]<<"\t";
+// 	for(int i=0;i<nonterminals.size();i++)
+// 		cout<<nonterminals[i]<<"\t";
+// 	cout<<"\n===========================================================================================\n";
+// 	for(int i=0;i<csimap.size();i++)
+// 	{
+// 		cout<<csimap<<""
+// 		if(csimap[i].state == 1)
+// 			cout<<""<<
+// 	}
+// }
+
+
 class CSICollection
 {
 	public:
 		vector<CSI> csis;
+		int returnsize()
+		{
+			return csis.size();
+		}
+
 		int addcsi(CSI c)
 		{
 			for(int i=0;i<csis.size();i++)
@@ -239,7 +286,14 @@ class CSICollection
 			csis.push_back(c);
 			return 0;
 		}
-		
+
+		int checkifexsists(CSI temp)
+		{
+			for(int i=0;i<csis.size();i++)
+				if(temp.compareCSI(csis[i]) == 1)
+					return i;
+			return 0;
+		}
 		void createnewcollection(int i)
 		{
 			int k=0;
@@ -254,17 +308,41 @@ class CSICollection
 				cout<<sstrings[j]<<endl;
 				cout<<"\nCreating new collection for the start string: "<<sstrings[j]<<endl;
 				CSI temp2 = csis[i].newcsi(sstrings[j]);
-				cout<<endl<<"Got new csi .....\n";
-				temp2.viewcsi();
-				cout<<endl;
-				temp.push_back(temp2);
-				CSIMap temp;
-				if(islower(sstrings[j][0]) == 0)
-					temp.addCSIMap(i,csis.size()+k,0,sstrings[j]);
+				int l=checkifexsists(temp2);
+				if(l == 0 && temp2.prods.size() > 0)
+				{
+					cout<<endl<<"Got new csi .....\n";
+					temp2.viewcsi();
+					cout<<endl;
+					temp.push_back(temp2);
+					CSIMap temp5;
+					if(islower(sstrings[j][0]) == 0)
+					{
+						if(temp2.prods.size() == 1 && temp2.prods[0].production.size() == temp2.prods[0].index)
+							temp5.addCSIMap(i,csis.size()+k,2,sstrings[j]);
+						else
+							temp5.addCSIMap(i,csis.size()+k,1,sstrings[j]);
+					}
+					else
+						temp5.addCSIMap(i,csis.size()+k,0,sstrings[j]);
+					k++;
+					csimap.push_back(temp5);
+				}
 				else
-					temp.addCSIMap(i,csis.size()+k,1,sstrings[j]);
-				k++;
-				csimap.push_back(temp);
+				{
+					CSIMap temp5;
+					cout<<"Already exsists................\n";
+					if(islower(sstrings[j][0]) == 0)
+					{
+						if(csis[l].prods.size() == 1 && csis[l].prods[0].production.size() == csis[l].prods[0].index)
+							temp5.addCSIMap(i,l,2,sstrings[j]);
+						else
+							temp5.addCSIMap(i,l,1,sstrings[j]);
+					}
+					else
+						temp5.addCSIMap(i,l,0,sstrings[j]);
+					csimap.push_back(temp5);
+				}
 			}
 			for(int j=0;j<temp.size();j++)
 					csis.push_back(temp[j]);
@@ -278,6 +356,23 @@ class CSICollection
 				cout<<"Collection "<<i<<endl;
 				csis[i].viewcsi();
 			}
+		}
+
+		void createset()
+		{
+			for(int i=0;i<csis.size();i++)
+			{
+				cout<<"Collection "<<i<<endl;
+				csis[i].viewcsi();
+				cout<<endl;
+				createnewcollection(i);
+				cout<<"\n==========================Done one collection creation =======================\n";
+				for(int j=0;j<csimap.size();j++)
+						csimap[j].viewCSIMap();
+				if(i == 3)
+					raise(SIGINT);
+			}
+			cout<<"Done Creating new sets\n";
 		}
 		
 };
@@ -304,8 +399,9 @@ int main()
 	temp.addproduction(tempprodcoll);
 	cout<<"CSI add production complete...........\n";
 	c.addcsi(temp);
-	c.viewcollection();
-	c.createnewcollection(0);
+	// c.viewcollection();
+	cout<<"\n\n=======================Starting CSI Collection Creation================\n";
+	c.createset();
 	for(int i=0;i<csimap.size();i++)
 		csimap[i].viewCSIMap();
 	return 0;
